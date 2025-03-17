7d753c92b045a6b7e580c6177b5e9c07a4371298
dharryc <dharrisoncook@gmail.com>
2025-03-17 17:43:32 -0600

    methods

--- ATM.cs
+++ ATM.cs
@@ -6,4 +6,37 @@ public class ATM
     private bool pinValidated;
     private BankServer bankServer = new();
     private string? currentCardNumber;
+
+    public ATM(BankServer server)
+    {
+        bankServer = server;
+    }
+    public void insertCard(string cardNumber)
+    {
+
+    }
+    public bool enterPIN()
+    {
+        return true;
+    }
+    public void requestAmount()
+    {
+
+    }
+    public void dispenseCash()
+    {
+
+    }
+    public void ejectCard()
+    {
+        cardInserted = false;
+    }
+    public void checkBalance()
+    {
+
+    }
+    public void getNextAction()
+    {
+        
+    }
 }
