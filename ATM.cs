41536d477f77d02b24412e99268d27d39e097742
dharryc <dharrisoncook@gmail.com>
2025-03-17 17:37:40 -0600

    fields

--- ATM.cs
+++ ATM.cs
@@ -1,1 +1,9 @@
 ﻿namespace ATM;
+
+public class ATM
+{
+    private bool cardInserted;
+    private bool pinValidated;
+    private BankServer bankServer = new();
+    private string? currentCardNumber;
+}
