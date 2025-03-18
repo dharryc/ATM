c40b5189d5b3ce5ff8c8378cee6e82bb79b19d79
landencaballero10 <landen.caballero@students.snow.edu>
2025-03-18 14:00:54 -0600

    finished?

--- BankAccount.cs
+++ BankAccount.cs
@@ -6,16 +6,20 @@ class BankAccount
 
     bool hasSufficientFunds(double amount)
     {
-        
+        if (amount > balance) return false;
+
+        return true;
     }
 
     bool Withdraw(double amount)
     {
+        balance -= amount;
 
+        return true;
     }
 
     double GetBalance()
     {
-
+        return balance;
     }
 }
