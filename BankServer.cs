1ad1a3db50fed42554acc8e0111e200ef59e4da5
Mapkyc18 <marcusgaldino3@gmail.com>
2025-03-24 11:19:48 -0600

    Methods Implemented

--- BankServer.cs
+++ BankServer.cs
@@ -1,51 +1,48 @@
 using System.Net.NetworkInformation;
 
-/*BankServer                                                                                                                    |
-+----------------------------------------------------------------------------------------------+
-| - validCards: Dictionary<string, (int pin, BankAccount account)>                             |
-+----------------------------------------------------------------------------------------------+
-| + BankServer(initialCards: Dictionary<string, (int pin, BankAccount account)>      |
-| + verifyCard(cardNumber: string) : bool                                                                       |
-| + verifyPIN(cardNumber: string, pin: int) : bool                                                           |
-| + processTransaction(cardNumber: string, amount: double) : bool                            |
-| + checkBalance(cardNumber: string) : double*/
 namespace ATM
 {
    public class BankServer
 
     {
-        BankAccount account = new BankAccount(1000);
     
-        public Dictionary<string, (int pin, BankAccount account)> validCards;
+        private Dictionary<string, (int pin, BankAccount account)> ValidCards;
   
-        public BankServer(Dictionary<string, (int pin, BankAccount account)> initialCards)
+        public BankServer(Dictionary<string, (int pin, BankAccount account)> initialCards = null)
         {
-            validCards = initialCards;
+            ValidCards = initialCards;
         
         }
         public bool verifyCard(string cardNumber)
         {
-            return validCards.ContainsKey(cardNumber);
+            return ValidCards.ContainsKey(cardNumber);
         }
         public bool verifyPIN(string cardNumber, int pin)
         {
-            if(validCards.ContainsKey(cardNumber))
+            if (ValidCards.TryGetValue(cardNumber, out var cardInfo))
             {
-                var storePin = validCards[cardNumber].pin;
-                return storePin == pin;
+                return cardInfo.pin == pin;
             }
             return false;
         }
         public bool processTransaction(string cardNumber, double amount)
         {
-            if(account.hasSufficientFunds(amount))
+            if (ValidCards.TryGetValue(cardNumber, out var cardInfo))
             {
-                return 
+                if (cardInfo.account.hasSufficientFunds(amount))
+                {
+                    return cardInfo.account.Withdraw(amount);
+                }
             }
+            return false;
         }
         public double checkBalance(string cardNumber)
         {
-            return 12;
+            if (ValidCards.TryGetValue(cardNumber, out var cardInfo))
+            {
+                return cardInfo.account.GetBalance();
+            }
+            return 0;
         }
 
     }
