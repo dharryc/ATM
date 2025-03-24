2768e598bacb8da811e69a983ed2a02899a0fdcf
dharryc <dharrisoncook@gmail.com>
2025-03-24 11:03:03 -0600

    testing

--- BankServer.cs
+++ BankServer.cs
@@ -13,14 +13,12 @@ namespace ATM
    public class BankServer
 
     {
-        BankAccount account = new BankAccount(1000);
     
-        public Dictionary<string, (int pin, BankAccount account)> validCards;
+        private Dictionary<string, (int, BankAccount)> validCards;
   
-        public BankServer(Dictionary<string, (int pin, BankAccount account)> initialCards)
+        private BankServer(Dictionary<string, (int, BankAccount)> initialCards)
         {
             validCards = initialCards;
-        
         }
         public bool verifyCard(string cardNumber)
         {
@@ -30,21 +28,22 @@ namespace ATM
         {
             if(validCards.ContainsKey(cardNumber))
             {
-                var storePin = validCards[cardNumber].pin;
+                var storePin = validCards[cardNumber].Item1;
                 return storePin == pin;
             }
             return false;
         }
         public bool processTransaction(string cardNumber, double amount)
         {
-            if(account.hasSufficientFunds(amount))
+            if(validCards[cardNumber].Item2.hasSufficientFunds(amount))
             {
-                return 
+                return true;
             }
+            return false;
         }
         public double checkBalance(string cardNumber)
         {
-            return 12;
+            return validCards[cardNumber].Item2.GetBalance();
         }
 
     }
